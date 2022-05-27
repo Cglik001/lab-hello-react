14c9de4129b1e998aa7a723247cf1ae238511f8d
christian landsberger Cglik001@gmail.com <cglik001@gmail.com>
2022-05-27 14:45:59 -0300

    lab-hello-react done

---
+++ hello-react-lab/src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import ironhackLogo from './assets/ironhack-logo-xs.png'
+import icon1 from './assets/icon1.png'
+import icon2 from './assets/icon2.png'
+import icon3 from './assets/icon3.png'
+import icon4 from './assets/icon4.png'
+import menuIcon from './assets/menu-top-xs.png'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-hero">
+        <nav>
+          <img src={ironhackLogo} alt="logo" className='logo' />
+          <img src={menuIcon} alt="menu icon"/>
+        </nav>
+        <h1>Say hello to ReactJS</h1>
+        <p>You will learn how to use the most popular frontend library, and become a super Ninja developer.</p>
+        <button>Awesome!</button>
+      </div>
+      <div className="App-features">
+        <div>
+          <img src={icon1} alt="icon1" className='icon' />
+          <h2>Declarative</h2>
+          <p>React makes it painless to create interactive UIs.</p>
+        </div>
+        <div>
+          <img src={icon2} alt="icon2" className='icon' />
+          <h2>Components</h2>
+          <p>Build encapsulated components that manage their state.</p>
+        </div>
+        <div>
+          <img src={icon3} alt="icon3" className='icon' />
+          <h2>Single-Way</h2>
+          <p>A set of immutable values are passed to the component's</p>
+        </div>
+        <div>
+          <img src={icon4} alt="icon4" className='icon' />
+          <h2>JSX</h2>
+          <p>Statically-typed designed to run on mondern browsers</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
